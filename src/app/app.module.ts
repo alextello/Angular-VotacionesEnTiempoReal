169781b27a225cdbece92b92962bcc3ffd5de896
unknown <alextello1@hotmail.com>
2020-04-16 17:35:34 -0600

    configuracion de modulo para componentes y diseño de navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { GotyComponent } from './pages/goty/goty.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, InicioComponent, GotyComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
